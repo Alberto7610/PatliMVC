a822a7286e7ac1fe4622efeed928d97acac51e67
Alberto Torres <alberto_tv@hotmail.com>
2023-11-27 18:24:53 -0600

    Creacion del proyecto

---
+++ PatliMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatliMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
