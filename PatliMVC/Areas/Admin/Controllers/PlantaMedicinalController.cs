57e1a2a7bae489a7d1ef3752ff50489b218b3040
Alberto Torres <alberto_tv@hotmail.com>
2023-12-01 18:50:42 -0600

    Vista index PlantaMedicinal

---
+++ PatliMVC/Areas/Admin/Controllers/PlantaMedicinalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatliMVC.AccesoDatos.Repositorio.IRepositorio;
+
+namespace PatliMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PlantaMedicinalController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public PlantaMedicinalController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = Json(new { data = await _unidadTrabajo.PlantaMedicinal.ObtenerTodosAsync() });
+            return todos;
+        }
+        #endregion
+    }
+}
